f14b7a8bf5d626a83eb267a09f1f3af314270bd3
Jardson Rainerio <48558649+jardsonrainerio@users.noreply.github.com>
2024-10-10 16:10:46 -0400

    add auth

--- app.py
+++ app.py
@@ -3,10 +3,10 @@ from flask import Flask, render_template
 from db import init_db
 from cliente import cliente_bp
 from fornecedor import fornecedor_bp
-
+from auth import auth_bp  # Importa o Blueprint de autenticação
 
 app = Flask(__name__)
-app.secret_key = 'sua_chave_secreta_aqui'  # Recomendado usar variáveis de ambiente para isso
+app.secret_key = 'sua_chave_secreta_aqui'  # Necessário para usar flash messages e sessões
 
 # Inicializa o banco de dados
 init_db()
@@ -14,6 +14,7 @@ init_db()
 # Registra os Blueprints
 app.register_blueprint(cliente_bp, url_prefix='/cliente')
 app.register_blueprint(fornecedor_bp, url_prefix='/fornecedor')
+app.register_blueprint(auth_bp)  # Registra o Blueprint de autenticação
 
 # Rota principal
 @app.route('/')
